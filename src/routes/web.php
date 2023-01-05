067267e3db6e254a8efdbef31648552eec8ff8c8
Syed Araib Raza <araibraza@gmail.com>
2023-01-05 15:10:55 +0500

    .

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,14 +1,10 @@
 <?php
 
-
-
-// Route::group(['namespace'=>'Bitfumes\Contact\Http\Controllers'], function() {
-
 use Illuminate\Support\Facades\Route;
 use Araib\Contact\Http\Controllers\ContactController;
 
     Route::get('contact', [ContactController::class, 'index']) ->name('contact');
 
     Route::post('contact', [ContactController::class, 'send']);
-// });
+
 
